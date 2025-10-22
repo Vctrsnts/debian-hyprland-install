5ddad81c00626c18ba4bf9bda9588762172770a1
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-10-22 23:46:40 +0200

    Add files via upload

--- custom-configs/hypr/autostart.sh
+++ custom-configs/hypr/autostart.sh
@@ -1,1 +1,12 @@
-s
+###############################################################################
+# Autostart applications
+###############################################################################
+
+# Agente de polkit (gestión de permisos administrativos)
+exec lxpolkit
+
+# WALLPAPER
+# wallpaper = monitor, ~/.config/backgrounds/Wallpaper_01.jpg
+
+
+
