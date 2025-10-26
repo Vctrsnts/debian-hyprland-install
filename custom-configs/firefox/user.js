fd9c5e2aab920b217ffa08000eb7e44175e305da
Victor <59069334+Vctrsnts@users.noreply.github.com>
2025-10-26 19:36:03 +0000

    Add files via upload

--- custom-configs/firefox/user.js
+++ custom-configs/firefox/user.js
@@ -1,1 +1,5 @@
-
+// ===== Configuración de aceleración por hardware =====
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.rdd-ffmpeg.enabled", true);
+user_pref("gfx.webrender.all", true);
+user_pref("layers.acceleration.force-enabled", true);
